980e4141de2a4980cf4fc7890f6e8dd7ce224ed4
pablo <chayka.pavlik@mail.ru>
2025-05-05 00:01:58 +0700

    Переделал отображение списка задач на ListView, отредактировал отображение на всех страницах (с размеров в натуральных числах на размеры в пропорциях от экрана)

--- MentalHealthApp/Planner.xaml.cs
+++ MentalHealthApp/Planner.xaml.cs
@@ -4,94 +4,37 @@ namespace MentalHealthApp;
 
 public partial class Planner : ContentPage
 {
-
-	public Planner()
-	{
-		InitializeComponent();
-	}
-    private IView newTask ()
+    List<Task> tasks = new List<Task>();
+    public Planner()
     {
-        Grid grid = (new Grid
+        InitializeComponent();
+        tasks = new List<Task>()
         {
-            Margin = new Thickness(16, 4, 12, 4),
-            ColumnSpacing = 12,
-            RowDefinitions =
-            {
-                new RowDefinition(),
-                new RowDefinition()
-            },
-            ColumnDefinitions =
-            {
-                new ColumnDefinition(WidthRequest = 12),
-                new ColumnDefinition(WidthRequest = 242),
-                new ColumnDefinition(WidthRequest = 10),
-                new ColumnDefinition(WidthRequest = 24),
-                new ColumnDefinition(WidthRequest = 24)
-            }
-
-        });
-        grid.Add(new Ellipse
-        {
-            WidthRequest = 12,
-            HeightRequest = 12,
-            Fill = SolidColorBrush.Gray,
-            HorizontalOptions = LayoutOptions.Start
-        });
-        grid.Add(new Label
-        {
-            Text = "Почитать Преступление и Наказание",
-            FontFamily = "Roboto",
-            FontSize = 14,
-            LineBreakMode = LineBreakMode.TailTruncation,
-            Margin = new Thickness(0, 2, 0, 0)
-        }, 1, 0);
-        grid.Add(new Image
-        {
-            Source = "cross_icon.png",
-
-        }, 2, 0);
-        grid.Add(new Image
-        {
-            Source = "threedots_icon.png",
-
-        }, 3, 0);
-        grid.Add(new CheckBox
-        {
-            Color = Color.FromArgb("#FF808080"),
-            WidthRequest = 24,
-            HeightRequest = 24
-
-        }, 4, 0);
-        grid.Add(new Label
-        {
-            Text = "18:30",
-            FontFamily = "Roboto",
-            FontSize = 14,
-        }, 1, 1);
-
-        Border border = new Border
-        {
-            StrokeShape = new RoundRectangle { CornerRadius = 20 },
-            HeightRequest = 58,
-            //Content = grid,
-
+            new Task {
+            TextTask = "Прочитать преступление и Наказание",
+            TimeOfTask = "18:30"},
+            new Task {
+            TextTask = "Заехать в магазин за продуктами",
+            TimeOfTask = ""},
         };
-        return border;
+
+        listOfTasks.ItemsSource = tasks;
+        BindingContext = this;
     }
+
     private void AddNewTask_Clicked(object sender, TappedEventArgs e)
     {
-        //IView border = newTask();
-
-        //TasksStackLayout.Children.Append(border);
-
+        tasks.Add(new Task
+        {
+            TextTask = "Прочитать преступление и Наказание",
+            TimeOfTask = "18:30"
+        });
+        //listOfTasks.ItemsSource = tasks;
     }
     public class Task
     {
-        public Ellipse ColorMark { get; set; }
         public string TextTask { get; set; }
-        public Image Cross { get; set; }
-        public Image ThreeDots { get; set; }
-        public CheckBox IsCompleted { get; set; }
         public string TimeOfTask { get; set; }
     }
+
 }
