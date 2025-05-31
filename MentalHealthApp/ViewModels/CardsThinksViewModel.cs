3879b0120a9feeefe14dbadf854733de3c39ed46
Pavel <chayka.pasha32@mail.ru>
2025-05-31 16:19:17 +0700

    Добавил добавление мыслей в избранное

--- MentalHealthApp/ViewModels/CardsThinksViewModel.cs
+++ MentalHealthApp/ViewModels/CardsThinksViewModel.cs
@@ -3,6 +3,7 @@ using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using MentalHealthApp.Models;
 using Microsoft.Maui.Controls.Shapes;
+using SQLiteNetExtensionsAsync.Extensions;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 
@@ -31,6 +32,17 @@ namespace MentalHealthApp.ViewModels
             var cards = await App.Database.GetListOfThinks(STName);
             ThCards = new ObservableCollection<PosThModel>(cards.Thinks);
 
+        }
+
+        [RelayCommand]
+        public async void PosThinkFavouriteStatusUpdate(object id)
+        {
+            PosThModel thinkModel = await App.Database.Connection.GetWithChildrenAsync<PosThModel>((int)id);
+            PosThModel think = ThCards.Where(x=>x.ThinkID==thinkModel.ThinkID).First();
+            int index = ThCards.IndexOf(think);
+            think.isFavourite = (think.isFavourite == 0) ? 1 : 0;
+            ThCards[index] = think;
+            await App.Database.Connection.UpdateWithChildrenAsync(think);
         }
 
     }
